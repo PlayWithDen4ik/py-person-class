f315fcfb0ea0845ed6e56a72d274a100772cd60e
PlayWithDen4ik <velichkodenis34@gmail.com>
2025-06-09 10:39:31 +0300

    Solution with simplification of verification conditions

--- app/main.py
+++ app/main.py
@@ -13,15 +13,8 @@ def create_person_list(people: list) -> list:
     for person in people:
         Person(person["name"], person["age"])
     for one in people:
-        if "wife" in one and one["wife"] is not None:
+        if one.get("wife"):
             Person.people[one["name"]].wife = Person.people[one["wife"]]
-        if "husband" in one and one["husband"] is not None:
+        if one.get("husband"):
             Person.people[one["name"]].husband = Person.people[one["husband"]]
     return [Person.people[one["name"]] for one in people]
-
-
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
